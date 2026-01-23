f9139f3cebab271cb8ab07177b63d273bc35f23d
Cheng Xin Lin <159232785+xinlin2@users.noreply.github.com>
2026-01-23 16:46:41 +0800

    remove eslint disable

--- frontend/src/pages/GamesQuizzes/GamesQuizPage.jsx
+++ frontend/src/pages/GamesQuizzes/GamesQuizPage.jsx
@@ -49,9 +49,7 @@ const GamesQuizPage = () => {
   // Quiz states
   const navigate = useNavigate();
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  // eslint-disable-next-line
   const [questions, setQuestions] = useState(quizData?.quiz || []);
-  // eslint-disable-next-line
   const [loading, setLoading] = useState(false);
   const [answers, setAnswers] = useState({});
   const [selectedOption, setSelectedOption] = useState(null);
